b77484b51735dd54ae3777d83e7b5e23a7e83820
SaltyInter <mshiroma01@gmail.com>
2024-07-08 19:26:16 -0700

    Add lambda_function for graphql mutation for getting needs wants and savings value

---
+++ lambda/resolvers/transaction_nws_mutation/lambda_function.py
@@ -0,0 +1,29 @@
+import boto3
+from decimal import Decimal
+import hashlib
+
+DYNAMODB_TABLE_NAME = 'TransactionTable'
+
+def lambda_handler(event, context):
+    # Extract input data from the GraphQL event
+    input_data = event.get('arguments', {}).get('input', {})
+    
+    # Convert float values to Decimal types
+    for key, value in input_data.items():
+        if isinstance(value, float):
+            input_data[key] = Decimal(str(value))
+    
+    # Prepare the item to be added to DynamoDB
+    item = {}
+    for key in input_data:
+        item[key] = input_data[key]
+    item_string = ''.join(str(item.get(field, '')) for field in sorted(item.keys()))
+    item['hash'] = hashlib.sha256(item_string.encode()).hexdigest()
+    
+    # Add the item to DynamoDB
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(DYNAMODB_TABLE_NAME)
+    response = table.put_item(Item=item)
+    
+    # Return the response
+    return item
